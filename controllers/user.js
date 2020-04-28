6b84d17370dfda845216f5b89697c81e130f4ea2
An Nguyen <saulnguyen@macOSs-MacBook-Pro.local>
2020-04-28 08:50:09 +0700

    [CORE] Using await async

--- controllers/user.js
+++ controllers/user.js
@@ -20,78 +20,63 @@ const transporter = Nodemailer.createTransport({
     }
 });
 
-exports.getUserInfo = (req, res, next) => {
+exports.getUserInfo = async (req, res, next) => {
     ValidatorUtil.catchValidation(req);
     const requestUserId = req.params.userId;
-    console.log(requestUserId);
-
-    User.findOne({ _id: requestUserId })
-        .then(user => {
-            if (!user) {
-                CommonError.throwError401(Define.errUserNotExists);
-            }
-            return user;
-        })
-        .then(user => {
-            Response.send(res, ModelUtil.getUser(user));
-        })
-        .catch(err => next(err));
+    try {
+        const user = await User.findOne({ _id: requestUserId })
+        if (!user) {
+            CommonError.throwError401(Define.errUserNotExists);
+        }
+        Response.send(res, ModelUtil.getUser(user));
+    } catch (err) { next(err) };
 }
 
-exports.putUpdateUserInfo = (req, res, next) => {
+exports.putUpdateUserInfo = async (req, res, next) => {
     ValidatorUtil.catchValidation(req);
     const userId = req.userId;
     const name = req.body.name;
     const files = req.files;
     const hasAvatar = (files && files.avatar && files.avatar[0]);
     let avatarUrl = hasAvatar ? files.avatar[0].path : null;
+    try {
+        const user = await User.findOne({ _id: userId })
+        if (!user) {
+            throw Error(Define.errUserNotExists);
+        }
 
-    User.findOne({ _id: userId })
-        .then(user => {
-            if (!user) {
-                throw Error(Define.errUserNotExists);
-            }
+        if (hasAvatar) {
+            console.log(user.avatar);
+            FileHelper.deleteFile(user.avatar);
+            user.avatar = avatarUrl;
+        }
 
-            if (hasAvatar) {
-                console.log(user.avatar);
-                FileHelper.deleteFile(user.avatar);
-                user.avatar = avatarUrl;
-            }
+        if (name) {
+            user.name = name;
+        }
 
-            if (name) {
-                user.name = name;
-            }
-
-            return user.save();
-        })
-        .then(user => {
-            Response.send(res, ModelUtil.getUser(user));
-        })
-        .catch(err => next(err));
+        await user.save();
+        Response.send(res, ModelUtil.getUser(user));
+    } catch (err) { next(err) };
 }
 
-exports.postForgotPassword = (req, res, next) => {
+exports.postForgotPassword = async (req, res, next) => {
     ValidatorUtil.catchValidation(req);
     const email = req.body.email;
     const newPassword = PasswordRandom(8);
-    User.findOne({ email: email })
-        .then(user => {
-            if (!user) {
-                throw Error(Define.errUserNotExists);
-            }
+    try {
+        const user = await User.findOne({ email: email })
+        if (!user) {
+            throw Error(Define.errUserNotExists);
+        }
 
-            return Bcrypt
-                .hash(newPassword, Config.PASSWORD_HASH_SAIL)
-                .then(hashedPw => {
-                    user.password = hashedPw;
-                    return user.save();
-                })
-        })
-        .then(user => {
-            sendForgotPasswordEmail(email, newPassword, user.name);
-            Response.message(res, Define.mesNewPasswordSent);
-        })
-        .catch(err => next(err));
+        const hashedPw = await Bcrypt.hash(newPassword, Config.PASSWORD_HASH_SAIL)
+        user.password = hashedPw;
+        await user.save();
+
+        sendForgotPasswordEmail(email, newPassword, user.name);
+        Response.message(res, Define.mesNewPasswordSent);
+    } catch (err) { next(err) };
 }
 
 const sendForgotPasswordEmail = (email, newPassword, name) => {
@@ -107,38 +92,30 @@ const sendForgotPasswordEmail = (email, newPassword, name) => {
         });
 }
 
-exports.putChangePassword = (req, res, next) => {
+exports.putChangePassword = async (req, res, next) => {
     ValidatorUtil.catchValidation(req);
     const oldPassword = req.body.oldPassword;
     const newPassword = req.body.newPassword;
     const userId = req.userId;
-    let loadedUser;
+    try {
+        const user = await User.findOne({ _id: userId })
 
-    User.findOne({ _id: userId })
-        .then(user => {
-            if (!user) {
-                throw Error(Define.errUserNotExists);
-            }
-            loadedUser = user;
-            console.log(oldPassword);
-            return Bcrypt.compare(oldPassword, user.password);
-        })
-        .then(isEqual => {
-            if (!isEqual) {
-                const error = new Error(Define.errPasswordWrong);
-                error.statusCode = 401;
-                throw error;
-            }
+        if (!user) {
+            throw Error(Define.errUserNotExists);
+        }
 
-            return Bcrypt
-                .hash(newPassword, Config.PASSWORD_HASH_SAIL)
-                .then(hashedPw => {
-                    loadedUser.password = hashedPw;
-                    return loadedUser.save();
-                })
-        })
-        .then(user => {
-            Response.send(res, ModelUtil.getUser(user));
-        })
-        .catch(err => next(err));
+        const isEqual = await Bcrypt.compare(oldPassword, user.password);
+        if (!isEqual) {
+            const error = new Error(Define.errPasswordWrong);
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const hashedPw = await Bcrypt.hash(newPassword, Config.PASSWORD_HASH_SAIL)
+
+        user.password = hashedPw;
+        await user.save();
+
+        Response.send(res, ModelUtil.getUser(user));
+    } catch (err) { next(err) };
 }
