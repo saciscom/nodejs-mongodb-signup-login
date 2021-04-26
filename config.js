bb1d66e80f80442a50ddf016d34ea2ec9acd0187
An Nguyen <saulnguyen@gmail.com>
2021-04-26 08:34:23 +0700

    [Config] Update config

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // project config
-exports.APP_NAME = 'Authenticate App';
-exports.MONGODB_URI = 'Your mongodb uri'; // Sample: mongodb+srv://mongouser:SamplePassword@cluster0-ppbsp.mongodb.net/authenticate
-exports.JWT_SECRET = 'Your JWT secret'; //This is used to sign and verify JWT tokens, replace with your own secret, it can be any string
+exports.APP_NAME = 'Alasto';
+exports.MONGODB_URI = 'mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false/alasto';
+exports.JWT_SECRET = 'WhatIsMySecret???';
 
 // user model config
 exports.PASSWORD_HASH_SAIL = 12;
 exports.PASSWORD_MIN_LENGTH = 8;
-exports.TOKEN_EXPIRES_IN = '30d'; // Expressed in seconds or a string describing a time span [zeit/ms](https://github.com/zeit/ms.js).  Eg: 60, "2 days", "10h", "7d"
+exports.TOKEN_EXPIRES_IN = '30d';
 
 // Gmail service
 // Using for setup forgot password mail.
